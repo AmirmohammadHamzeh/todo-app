7e917b2de031db03c43344b6b92e9c086a1349bc
tandorost <amrimohammadhamzehi@gmail.com>
2025-10-05 05:12:21 +0330

    feat(todos): add todos app with models, serializers, views, and URLs

---
+++ todos/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Todo
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ['id', 'title', 'description', 'is_completed', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at']
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        return Todo.objects.create(user=user, **validated_data)
